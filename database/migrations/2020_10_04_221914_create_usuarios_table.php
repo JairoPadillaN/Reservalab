7fc8307a0bec71e78bfc7c59652caaa8b4210ed6
EberPalma <al221911577@gmail.com>
2020-10-04 17:38:04 -0500

    Creacion de tabla usuarios EPC

---
+++ database/migrations/2020_10_04_221914_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrement('id');
+            $table->String('nombre');
+            $table->String('apellidos');
+            $table->String('email');
+            $table->String('contrasenia');
+            $table->int('telefono');
+            $table->foreign('id_analisis')->references('id')->on('analisis');
+            $table->foreign('id_medico')->references('id')->on('doctores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
