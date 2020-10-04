38379d410bf371e79251cfc50572242c99ccf66c
JairoPadillaN <al221910005@gmail.com>
2020-10-04 16:43:49 -0500

    Tabla Analisis

---
+++ database/migrations/2020_10_04_213804_create_analisis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnalisisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('analisis', function (Blueprint $table) {
+            $table->bigIncrement('id');
+            $table->String('nombre');
+            $table->String('tipoAnalisis');
+            $table->String('descripcion');
+            $table->int('precio')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('analisis');
+    }
+}
